919291e409ab5f922bcbcd82790765f81442ba09
Sendhil Panchadsaram <sendhilp@gmail.com>
2020-09-21 12:37:24 -0700

    Some tweaks to node deletion.
    * Checking for file existence.
    * Adding support for deleting a folder.
    * Added a second prompt when deleting a folder.

--- src/deleteChildNode.ts
+++ src/deleteChildNode.ts
@@ -1,24 +1,43 @@
 import * as vscode from 'vscode';
-import * as path from 'path';
 import { getCurrentlySelectedFilePath } from './utility';
-
 
 export async function deleteChildNode(context: vscode.ExtensionContext) {
 	const currentlySelectedFile = await getCurrentlySelectedFilePath();
+	let isDirectory = false;
+	try {
+		const fileStats = await vscode.workspace.fs.stat(vscode.Uri.file(currentlySelectedFile));
+		isDirectory = fileStats.type === vscode.FileType.Directory;
+	} catch(_) {
+		// File doesn't exist, exit out
+		await vscode.window.showErrorMessage(`Error - file ${currentlySelectedFile} does not exist.`);
+		return;
+	}
 
-	const confirmation = await vscode.window.showInputBox({
-		prompt: `Are you sure you wish to delete : ${currentlySelectedFile} (yN): ?`,
+	if (!await confirmDeletion(`Are you sure you wish to delete : ${currentlySelectedFile} (yN): ?`)) {
+		return;
+	}
+
+	if (isDirectory) {
+		// TODO: Add setting to skip this
+		if (await confirmDeletion(`Are you sure you wish to delete folder : ${currentlySelectedFile}?`)) {
+			await vscode.workspace.fs.delete(vscode.Uri.file(currentlySelectedFile), {recursive: true});
+			await vscode.window.showInformationMessage(`Deleted folder :${currentlySelectedFile}.`);
+		}
+	} else {
+		await vscode.workspace.fs.delete(vscode.Uri.file(currentlySelectedFile));
+		await vscode.window.showInformationMessage(`Deleted file :${currentlySelectedFile}.`);
+	}
+
+}
+
+async function confirmDeletion(message: string ): Promise<Boolean> {
+	const confirmation = (await vscode.window.showInputBox({
+		prompt: message,
 		validateInput: text => {
 			const lowerCaseText = text.toLocaleLowerCase();
 			return ['y', 'n', 'yes', 'no'].indexOf(lowerCaseText) === -1 ? 'y or n' : null;
 		}
-	});
+	}))?.trim();
 
-	if (confirmation?.toLowerCase().indexOf("y") === -1) {
-		return;
-	}
-
-	const dirName = path.dirname(currentlySelectedFile);
-	await vscode.workspace.fs.delete(vscode.Uri.file(currentlySelectedFile));
-	vscode.window.showInformationMessage(`Deleted :${currentlySelectedFile} located in : ${dirName}`);
+	return confirmation?.toLowerCase().indexOf("y") !== -1;
 }
