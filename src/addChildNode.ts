835546fa46b76398ba3a07ae6720f3c5f3e9bb82
Sendhil Panchadsaram <sendhilp@gmail.com>
2020-09-21 12:35:22 -0700

    Few tweaks to adding a childnode
    * Support for creating directories.
    * Checks that the path of the new node falls under a workspace folder.
    * Added missing `await`'s.
    * Prompts for a workspace root when no file selected.

--- src/addChildNode.ts
+++ src/addChildNode.ts
@@ -1,16 +1,48 @@
 import * as vscode from 'vscode';
 import * as path from 'path';
-import { getCurrentlySelectedFilePath } from './utility';
+import { getCurrentlySelectedFilePath, doesPathBelongToWorkspace } from './utility';
 
+export async function addChildNode() {
+	let rootFolder:vscode.WorkspaceFolder | undefined;
+	if (!vscode.window.activeTextEditor) {
+		rootFolder = await vscode.window.showWorkspaceFolderPick({ placeHolder: "Select a root folder."});
+		if (!rootFolder) {
+			await vscode.window.showInformationMessage("No folder selected");
+			return;
+		}
+	}
 
-export async function addChildNode(context: any) {
-	const baseName = await vscode.window.showInputBox({
-		value: '',
+	let startingFilePath:string | undefined;
+	if (rootFolder) {
+		startingFilePath = `${rootFolder.uri.fsPath}/`;
+	} else {
+		startingFilePath = `${path.dirname(await getCurrentlySelectedFilePath())}/`;
+	}
+
+	const newFilePath = (await vscode.window.showInputBox({
+		value: startingFilePath,
 		placeHolder: 'Enter filename',
-	});
+		valueSelection: [startingFilePath.length, startingFilePath.length]
+	}))?.trim();
 
-	const workingDirectory = path.dirname(await getCurrentlySelectedFilePath());
-	const fullFilePath = `${workingDirectory}/${baseName}`;
-	vscode.workspace.fs.writeFile(vscode.Uri.file(fullFilePath), new Uint8Array());
-	vscode.window.showInformationMessage(`Created : ${fullFilePath}`);
+	if (!newFilePath) {
+		return;	
+	}
+
+	if (!doesPathBelongToWorkspace(newFilePath)) {
+		await vscode.window.showErrorMessage(`${newFilePath} does not belong to a Workspace folder`);
+		return;
+	}
+
+	if (newFilePath.length > 0 && newFilePath[newFilePath.length - 1] === "/") {
+		// Create directory	
+		await vscode.workspace.fs.createDirectory(vscode.Uri.file(newFilePath));
+	} else {
+		// Create file
+		const filePath = vscode.Uri.file(newFilePath);
+		await vscode.workspace.fs.writeFile(filePath, new Uint8Array());
+		await vscode.workspace.openTextDocument(filePath);
+	}
+
+	await vscode.window.showInformationMessage(`Created : ${newFilePath}`);
 }
--- src/utility.ts
+++ src/utility.ts
@@ -1,4 +1,5 @@
 import * as vscode from 'vscode';
+import * as path from 'path';
 
 export async function getCurrentlySelectedFilePath(): Promise<string> {
 	let currentText = await vscode.env.clipboard.readText();
@@ -6,4 +7,25 @@ export async function getCurrentlySelectedFilePath(): Promise<string> {
 	let pathOfActiveFile = await vscode.env.clipboard.readText();
 	await vscode.env.clipboard.writeText(currentText);
 	return pathOfActiveFile;
+}
+
+// TODO: Make this configurable
+export function doesPathBelongToWorkspace(filePath:string): Boolean {
+	if (!vscode.workspace.workspaceFolders) {
+		return false;
+	}
+
+	// Source - https://stackoverflow.com/questions/37521893/determine-if-a-path-is-subdirectory-of-another-in-node-js
+	const isPathRelative = (workspaceRoot:string) : Boolean => {
+		let relative = path.relative(workspaceRoot, filePath);
+		return relative.length > 0 && !relative.startsWith('..') && !path.isAbsolute(relative);
+	};
+
+	for (let workspaceFolder of vscode.workspace.workspaceFolders) {
+		if (isPathRelative(workspaceFolder.uri.fsPath)) {
+			return true;
+		}
+	}
+
+	return false;
 }
