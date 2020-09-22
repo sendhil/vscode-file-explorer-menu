024d4b9e968d3f7582f380a5a9f2f15641f0a01b
Sendhil Panchadsaram <sendhilp@gmail.com>
2020-09-21 21:47:03 -0700

    Adjusting listNode - error if file doesn't exist.

--- src/listCurrentNode.ts
+++ src/listCurrentNode.ts
@@ -7,6 +7,13 @@ export async function listCurrentNode(context: vscode.ExtensionContext) {
 	// TODO: Add option to display in terminal
 	let currentFilePath = await getCurrentlySelectedFilePath();
 
+	try {
+		 await vscode.workspace.fs.stat(vscode.Uri.file(currentFilePath));
+	} catch (_) {
+		await vscode.window.showErrorMessage("Node does not exist.");
+		return;
+	}
+
 	const { stdout, stderr } = await exec(`ls -la ${currentFilePath}`);
 	stdout?.on("data", (output) => {
 		vscode.window.showInformationMessage(output);
